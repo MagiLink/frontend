588099412e945695aa1a2d57d46d459ae74f6da3
danewhitfield <danejwhitfield@gmail.com>
2023-02-25 13:40:40 +0000

    created a mock prompt and response

---
+++ src/data/prompts.js
@@ -0,0 +1,21 @@
+module.exports = [
+	{
+		prompt:
+			'Create a react component called SendButton that has a button with onclick functionality',
+		generated_code: `import React from 'react';
+
+                        const SendButton = () => {
+                            const handleClick = () => {
+                                alert('Button clicked!');
+                            };
+
+                            return (
+                                <button onClick={handleClick}>
+                                    Send
+                                </button>
+                            );
+                        };
+
+                        export default SendButton;`,
+	},
+];
