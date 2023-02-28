1c0ed41b4aad526e94bb660358b259f67b6f4f20
Bence Gadanyi <29914644+Beenyaa@users.noreply.github.com>
2023-02-28 21:52:36 +0000

    feat: more WIP

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -10,7 +10,7 @@ const FormModal = () => {
         type="button"
         onClick={() => setModalActivity(true)}
       >
-        Toggle modal
+        Share your work!
       </button>
       {isModalActive && (
         <div
@@ -42,81 +42,49 @@ const FormModal = () => {
               </button>
               <div className="px-6 py-6 lg:px-8">
                 <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-                  Sign in to our platform
+                  Component submission
                 </h3>
                 <form className="space-y-6" action="#">
                   <div>
                     <label
-                      for="email"
+                      for="component-name"
                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
-                      Your email
+                      Name
                     </label>
                     <input
-                      type="email"
-                      name="email"
-                      id="email"
+                      name="component-name"
+                      id="component-name"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                      placeholder="name@company.com"
+                      placeholder="Kim Jung Un's big red button"
                       required
                     ></input>
                   </div>
+
                   <div>
                     <label
-                      for="password"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      for="countries"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
-                      Your password
+                      Category
                     </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                      required
-                    ></input>
-                  </div>
-                  <div className="flex justify-between">
-                    <div className="flex items-start">
-                      <div className="flex items-center h-5">
-                        <input
-                          id="remember"
-                          type="checkbox"
-                          value=""
-                          className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                          required
-                        ></input>
-                      </div>
-                      <label
-                        for="remember"
-                        className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                      >
-                        Remember me
-                      </label>
-                    </div>
-                    <a
-                      href="#"
-                      className="text-sm text-blue-700 hover:underline dark:text-blue-500"
+                    <select
+                      id="countries"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     >
-                      Lost Password?
-                    </a>
+                      <option selected>Choose a category</option>
+                      <option value="Modal">Modal</option>
+                      <option value="CA">Canada</option>
+                      <option value="FR">France</option>
+                      <option value="DE">Germany</option>
+                    </select>
                   </div>
                   <button
                     type="submit"
                     className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
-                    Login to your account
+                    Share your component
                   </button>
-                  <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                    Not registered?{" "}
-                    <a
-                      href="#"
-                      className="text-blue-700 hover:underline dark:text-blue-500"
-                    >
-                      Create account
-                    </a>
-                  </div>
                 </form>
               </div>
             </div>
