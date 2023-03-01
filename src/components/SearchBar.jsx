ebd42d31f662bfa734e39058070d743a4f9b765a
Róbert Fjölnir Birkisson <rfb97@hotmail.com>
2023-03-01 14:30:10 +0100

    Added function to search button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -24,6 +24,7 @@ function SearchBar({ handleSearch }) {
 				/>
 				<button
 					className="absolute right-2.5 bottom-1.5 bg-transparent hover:bg-black/20 focus:outline-none rounded-xl px-3 py-1"
+					onClick={() => handleSearch(prompt)}
 				>
 					<svg aria-hidden="true" class="w-5 h-5 text-black dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
 				</button>
