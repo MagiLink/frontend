dfea287d8b9083bd49c221d55a63d8748e53b5e8
Dane Whitfield <danewhitfield@Danes-MacBook-Pro.local>
2023-03-01 15:23:11 +0000

    refactor generate POST req

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -4,26 +4,10 @@ const StateContext = createContext();
 
 export const ContextProvider = ({ children }) => {
 	const [prompt, setPrompt] = useState(
-		'Create a react component called SendButton that has a button with onclick functionality',
+		'Create a react component called SendButton that has a button with onclick functionality'
 	);
-	const [code, setCode] = useState(`() => {
-		return (
-			<ul>
-				<li>Item 1</li>
-				<li>Item 2</li>
-				<li>Item 3</li>
-			</ul>
-		);
-	}`);
-	// 	const [code, setCode] = useState(`() => {
-	// 	const handleClick = () => {
-	// 		alert('Button clicked!');
-	// 	};
-
-	// 	return <button onClick={handleClick}>Send</button>;
-	// }`);
+	const [code, setCode] = useState(``);
 	const [currentLink, setCurrentLink] = useState('generate');
-
 	const [isFormModalActive, setIsFormModalActive] = useState(false);
 
 	return (
